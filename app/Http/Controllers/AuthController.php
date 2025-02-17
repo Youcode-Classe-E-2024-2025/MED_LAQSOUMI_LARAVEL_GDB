da5fba8bc6b31242bb70a7f3732cc159d4f5d9bf
medlaq777 <m.laqsoumi@gmail.com>
2025-02-17 15:50:44 +0100

    make all necessary functions in AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,5 +6,34 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    
+    public function showRegistrationForm()
+    {
+        return view('auth.register');
+    }
+
+    
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([]); 
+    }
+
+    
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([]); 
+    }
+
+    
+    public function logout(Request $request)
+    {
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+    }
 }
