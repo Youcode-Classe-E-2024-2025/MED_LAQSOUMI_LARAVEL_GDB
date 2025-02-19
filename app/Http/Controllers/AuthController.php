9fde5c82172d6f3f79d1327d51c08087de0d1f8b
medlaq777 <m.laqsoumi@gmail.com>
2025-02-19 10:23:18 +0100

    Enhance dashboard function to include user data redirection

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,63 +12,63 @@ class AuthController extends Controller
  
     public function register(Request $request)
     {
-    if ($request->isMethod('POST')) {
-        $validatedData = $request->validate([]);
-        $validatedData['name'] = $request->input('name');
-        $validatedData['email'] = $request->input('email');
-        $validatedData['password'] = Hash::make($request->input('password'));
-        $validatedData['remember_token'] = Hash::make($request->input('email'));
-        $validatedData['role'] = 'user';
-        if(User::create($validatedData)){
-            return redirect()->route('login')->with('success', 'Registration successful!');
+        if ($request->isMethod('POST')) {
+            $validatedData = $request->validate([]);
+            $validatedData['name'] = $request->input('name');
+            $validatedData['email'] = $request->input('email');
+            $validatedData['password'] = Hash::make($request->input('password'));
+            $validatedData['remember_token'] = Hash::make($request->input('email'));
+            $validatedData['role'] = 'user';
+            if(User::create($validatedData)){
+                return redirect()->route('login')->with('success', 'Registration successful!');
+            } else {
+                return redirect()->back()->with('error', 'Registration failed!');
+            }
         } else {
-            return redirect()->back()->with('error', 'Registration failed!');
+            return view('auth.register');
         }
-    } else {
-        return view('auth.register');
-    }
     }
 
     public function login(Request $request)
     {
-    if ($request->isMethod('POST')) {
-        $validatedData = $request->validate([
-            'email' => 'required|email',
-            'password' => 'required',
-        ]);
-        $email = $request->input('email');
-        $password = $request->input('password');
-        $user = User::where('email', $email)->first();
-        if ($user && Hash::check($password, $user->password)) {
-            $request->session()->put('email', $email);
-            $request->session()->regenerate();
-            $request->session()->put('role', $user->role);
-            return redirect()->route('dashboard');
+        if ($request->isMethod('POST')) {
+            $validatedData = $request->validate([
+                'email' => 'required|email',
+                'password' => 'required',
+            ]);
+            $email = $request->input('email');
+            $password = $request->input('password');
+            $user = User::where('email', $email)->first();
+            if ($user && Hash::check($password, $user->password)) {
+                $request->session()->put('email', $email);
+                $request->session()->regenerate();
+                $request->session()->put('role', $user->role);
+                return redirect()->route('dashboard');
+            } else {
+                return redirect()->back()->with('error', 'Invalid login details!');
+            }
         } else {
-            return redirect()->back()->with('error', 'Invalid login details!');
+            return view('auth.login');
         }
-    } else {
-        return view('auth.login');
-    }
     }
     
     public function logout(Request $request)
     {
-    $request->session()->flush();
-    return redirect()->route('login')->with('success', 'See you later!');
+        $request->session()->flush();
+        return redirect()->route('login')->with('success', 'See you later!');
     }
 
     public function dashboard()
     {
-    if (session()->has('email')) {
-        $sessionRole = session()->get('role');
-        if ($sessionRole == 'admin') {
-            return view('dashboard.admin');
+        if (session()->has('email')) {
+            $sessionRole = session()->get('role');
+            if ($sessionRole == 'admin') {
+                return view('dashboard.admin', ['email' => session()->get('email'), 'role' => $sessionRole, 'name' => User::where('email', session()->get('email'))->first()->name]);
+            } else {
+                return view('dashboard.user', ['email' => session()->get('email'), 'role' => $sessionRole, 'name' => User::where('email', session()->get('email'))->first()->name]);
+            }
         } else {
-            return view('dashboard.user');
+            return redirect()->route('login')->with('error', 'Please login to access the dashboard!');
         }
-    } else {
-        return redirect()->route('login')->with('error', 'Please login to access the dashboard!');
-    }
     }
 }
