ff34b0accbc00315904601d857e640526a8f613a
medlaq777 <m.laqsoumi@gmail.com>
2025-02-20 21:08:17 +0100

    fix the cover of book and now books displayed with covers and also with icons view edit and delete

--- resources/views/dashboard/booksDisplay.blade.php
+++ resources/views/dashboard/booksDisplay.blade.php
@@ -3,15 +3,29 @@
         <div class="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             @foreach ($books as $book)
             <div class="bg-white p-4 rounded-lg shadow-md">
-                <h4 class="font-semibold">{{ $book->title }}</h4>
-                <p class="text-gray-600">By {{ $book->author }}</p>
-                <p class="text-gray-500">ISBN: {{ $book->isbn }}</p>
-                <div class="mt-4">
-                    {{-- <a href="{{ route('books.show', $book->id) }}"  --}}
-                       class="inline-block px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition duration-300">
-                        View Details
-                    </a>
-                </div>
+            <img src="{{ $book->cover }}" alt="{{ $book->title }}" class="w-full h-64 object-cover rounded-md">
+            <h4 class="font-semibold">{{ $book->title }}</h4>
+            <p class="text-gray-600">By {{ $book->author }}</p>
+            <p class="text-gray-500">ISBN: {{ $book->isbn }}</p>
+            <div class="mt-4 flex gap-4 items-center">
+                {{-- <p class="text-gray-600">Published on {{ $book->published_at->format('M d, Y') }}</p> --}}
+                <a href="" 
+                   class="inline-block px-4 py-2 text-blue-600 rounded-md">
+                   <i class="fas fa-eye"></i>
+                </a>
+                <a href="" 
+                   class="inline-block px-4 py-2 text-yellow-600 transition duration-300">
+                <i class="fas fa-edit"></i>
+                </a>
+                <form action="" method="POST" onsubmit="return confirm('Are you sure you want to delete this book?');">
+                @csrf
+                @method('DELETE')
+                <button type="submit" 
+                    class="inline-block px-4 py-2 text-red-600 transition duration-300">
+                    <i class="fas fa-trash"></i>
+                </button>
+                </form>
+            </div>
             </div>
             @endforeach
         </div>
