29d9ee10a8fd8857fa4168210d80160939c04aa1
medlaq777 <m.laqsoumi@gmail.com>
2025-02-21 12:07:41 +0100

    fix routing issues

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\StatisticsController;
 
-// SYSTEM AUTHENTICATION
+// AUTHENTICATION
 Route::get('/', function () {return view('/welcome');})->name('welcome');
 Route::get('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
@@ -16,6 +16,8 @@ Route::get('/dashboard', [AuthController::class, 'dashboard'])->name('dashboard'
 Route::get('/profile', [AuthController::class, 'profile'])->name('profile');
 Route::post('/profile/edit', [AuthController::class, 'editProfile'])->name('editProfile');
 
+
+// BOOKS
 Route::get('/books', [BookController::class, 'books'])->name('books');
 Route::get('/books/create', [BookController::class, 'createBook'])->name('create-book');
 Route::post('/books/create', [BookController::class, 'createBook']);
@@ -23,3 +25,5 @@ Route::get('/books/edit/{id}', [BookController::class, 'editBook'])->name('edit-
 Route::post('/books/edit/{id}', [BookController::class, 'editBook']);
 Route::get('/books/delete/{id}', [BookController::class, 'deleteBook'])->name('delete-book');
 Route::post('/books/delete/{id}', [BookController::class, 'deleteBook']);
+
+// STATISTICS
