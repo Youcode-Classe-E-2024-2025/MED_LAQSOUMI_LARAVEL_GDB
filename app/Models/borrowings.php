8e980c3eed4318d19e8978470faa7e1fd1711e5a
medlaq777 <m.laqsoumi@gmail.com>
2025-02-21 16:18:19 +0100

    make some functionality initial on the borrowings model

--- app/Models/borrowings.php
+++ app/Models/borrowings.php
@@ -6,5 +6,34 @@ use Illuminate\Database\Eloquent\Model;
 
 class borrowings extends Model
 {
-    //
+    protected $table = 'borrowings';
+    protected $fillable = ['book_id', 'user_id', 'borrowed_at', 'returned_at'];
+    public $timestamps = true;
+    public function book()
+    {
+        return $this->belongsTo('App\Models\books', 'book_id');
+    }
+    public function user()
+    {
+        return $this->belongsTo('App\Models\users', 'user_id');
+    }
+
+    public function scopeBorrowed($query)
+    {
+        return $query->whereNull('returned_at');
+    }
+
+    public function scopeReturned($query)
+    {
+        return $query->whereNotNull('returned_at');
+    }
+
+    public function borrowingsBookById($id)
+    {
+        return $this->where('book_id', $id)->borrowed()->get();
+    }
+    public function borrowingsBook()
+    {
+        return $this->hasMany('App\Models\books', 'book_id');
+    }
 }
